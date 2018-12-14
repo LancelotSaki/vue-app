d77060f582958e00b80c439e45b8d0d53d0f341b
Ganhx <1103418874@qq.com>
2018-12-14 18:24:55 +0800

    移除console修正

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,23 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const production = process.env.NODE_ENV === "production";
 module.exports = {
   lintOnSave: !production,
   productionSourceMap: false,
   configureWebpack: config => {
-    if (production) {// 为生产环境修改配置...
-      config.optimization.minimizer[0] =
-      new UglifyJsPlugin({
+    // 为生产环境修改配置...
+    if (production) {
+      config.optimization.minimizer[0] = new UglifyJsPlugin({
         uglifyOptions: {
           compress: {
             warnings: false,
             drop_debugger: true,
             drop_console: true, //console
-            pure_funcs: ['console.log']//移除console
-          },
+            pure_funcs: ["console.log"] //移除console
+          }
         },
         sourceMap: false,
-        parallel: true,
-      })
+        parallel: true
+      });
     } else {
       // 为开发环境修改配置...
     }
