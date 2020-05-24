6ccab4c265f1419e4cbc4e4388df6da47bc25e10
LancelotSaki <1103418874@qq.com>
2020-05-24 23:44:27 +0800

    添加报表功能

--- src/views/echarts/cmdb/host/Host.vue
+++ src/views/echarts/cmdb/host/Host.vue
@@ -1,21 +1,41 @@
 <template>
-  <div class="hostContainer">
+  <article class="hostContainer">
     <Header></Header>
-  </div>
+    <Main></Main>
+  </article>
 </template>
 <script>
   import Header from "./layout/Header";
+  import Main from "./layout/Main";
 export default {
   name: "host",
-  components: { Header},
+  components: { Header, Main},
 };
 </script>
 <style scoped>
+
   .hostContainer {
     overflow-x: hidden;
     overflow-y: auto;
     background: rgba(0, 0, 0, 0.85) !important;
     /* background: url("../../../../npage/cmdb/auto-find/topo/1.png") fixed no-repeat;*/
     background-size: cover;
+  }
+
+  header {
+    padding-top: 10px;
+    width: 100%;
+    height: 70px;
+    text-align: center;
+    color: rgba(253, 255, 255, 1);
+    font-weight: bolder;
+    position: relative;
+  }
+
+  main {
+    width: 300px;
+    height: auto;
+    position: relative;
+    margin-top: 70px;
   }
 </style>
--- src/views/echarts/cmdb/host/layout/Header.vue
+++ src/views/echarts/cmdb/host/layout/Header.vue
@@ -12,12 +12,13 @@
       </defs>
       <defs>
         <linearGradient id="deep_blue_line" x1="0%" y1="0%" x2="100%" y2="0%" gradientUnits="userSpaceOnUse">
-          <stop offset="0%" style="stop-color: rgba(36,99,136, 0.65);"/>
-          <stop offset="50%" style="stop-color: rgba(36,99,136, 1);"/>
-          <stop offset="100%" style="stop-color: rgba(36,99,136, 0.65);"/>
+          <stop offset="0%" style="stop-color: rgba(100,100,100,1);"/>
+          <stop offset="50%" style="stop-color: rgba(100,100,100,1);"/>
+          <stop offset="100%" style="stop-color: rgba(100,100,100,1);"/>
         </linearGradient>
       </defs>
-      <g stroke="url(#blue_line)" stroke-width="2">
+     <!-- g标签属于非形状元素-->
+      <g stroke="url(#blue_line)" stroke-width="2" stroke-dasharray="10">
         <title>broken line1</title>
         <!-- 线条颜色的渐变 -->
         <line x1="0%" y1="45%" x2="28%" y2="45%"/>
@@ -25,14 +26,15 @@
         <line x1="30%" y1="80%" x2="44.5%" y2="80%"/>
         <line x1="44.5%" y1="80%" x2="45.5%" y2="90%"/>
       </g>
-      <g stroke="url(#deep_blue_line)" stroke-width="2" fill="rgba(100,100,100,1)">
+      <!--stroke-dasharray 是用于设置虚线的属性-->
+      <g stroke="url(#deep_blue_line)" stroke-width="2" stroke-dasharray="10 2">
         <title>broken line2</title>
         <line x1="45%" y1="80%" x2="55%" y2="80%"/>
         <line x1="55%" y1="80%" x2="54%" y2="90%"/>
         <line x1="54%" y1="90%" x2="46%" y2="90%"/>
         <line x1="46%" y1="90%" x2="45%" y2="80%"/>
       </g>
-      <g stroke="url(#blue_line)" stroke-width="2">
+      <g stroke="url(#blue_line)" stroke-width="2" stroke-dasharray="10">
         <title>broken line3</title>
         <line x1="54.5%" y1="90%" x2="55.5%" y2="80%" stroke-linecap="round"/>
         <line x1="55%" y1="80%" x2="70%" y2="80%" stroke-linecap="round"/>
@@ -59,15 +61,6 @@
     position: absolute;
     left: 0;
     top: 0;
-  }
-  header {
-    padding-top: 10px;
-    width: 100%;
-    height: 70px;
-    text-align: center;
-    color: rgba(253, 255, 255, 1);
-    font-weight: bolder;
-    position: relative;
   }
 
   header > p {
